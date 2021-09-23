a3503cb23c90b85985f80d8319e46dea65037cff
J190_정현구 <jungheun99@naver.com>
2021-09-24 01:46:43 +0900

    [FEAT] 순한맛 페이지 생성
    매운맛 페이지에서 사용하는 js파일명 변경(cookie.js -> hot-cookie.js)
    mild-cookie.js 생성

---
+++ public/javascripts/mild-cookie.js
@@ -0,0 +1,51 @@
+const cookieWrapper = document.getElementById('fortuneCookie');
+const cookie = document.getElementById('cookie');
+const getNew = document.getElementById('new');
+const flip = document.getElementById('flip');
+const fortune = document.body.querySelector('.fortune');
+const numbers = document.body.querySelector('.numbers');
+const paper = document.body.querySelector('.paper-wrapper');
+const speed = 500;
+
+// Todo : fetch 사용해서 랜덤으로 말 뽑아오면 됨
+const fetchFortune = () => {
+    fetch('http://localhost:3000/ovenapi/read-one-random/1')
+        .then(response => response.json())
+        .then((data) => {
+            fortune.innerHTML = data.contents;
+            numbers.innerHTML = 'Lucky numbers : 7';
+        })
+}
+
+const showFortune = () => {
+  cookieWrapper.classList.add('open');
+  cookie.disabled = true;
+};
+
+fetchFortune();
+
+cookie.addEventListener('click', showFortune, false);
+
+flip.addEventListener('click', e => {
+  paper.classList.toggle('flip');
+  flip.classList.toggle('front');
+  flip.classList.toggle('back');
+});
+
+getNew.addEventListener('click', e => {
+  cookieWrapper.classList.add('clear');
+  window.setTimeout(() => {
+    paper.classList.remove('flip');
+    flip.className = 'small front';
+    cookieWrapper.classList.remove('clear');
+    cookieWrapper.classList.remove('open');
+    cookieWrapper.classList.add('drop');
+    window.setTimeout(() => {
+      cookieWrapper.classList.remove('drop');
+      fortune.innerHTML = '';
+      numbers.innerHTML = '';
+      fetchFortune();
+    }, speed);
+  }, speed);
+  cookie.disabled = false;
+}, false);
