912b0a5d44ac5e9f6aecea89103c1dfb5e2de9d0
alittlekitten <dlscjs8646@gmail.com>
2021-09-24 01:14:03 +0900

    [FIX] 헤더 위치 수정 및 페이지 연결작업
    헤더의 로고와 items를 누르면 설정된 페이지로 넘어갈 수 있도록 설정

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/fortune', function(req, res, next) {
+  res.render('fortune');
+});
+
 router.get('/mild', function(req, res, next) {
   res.render('mild');
 });
