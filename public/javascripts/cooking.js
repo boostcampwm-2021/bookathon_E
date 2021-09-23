c43c0897c202456719cacce6e6c269ddb7775be2
Subin Min <tnqls5417@naver.com>
2021-09-23 22:53:56 +0900

    [FEAT] cooking 페이지 이벤트 및 애니메이션 구현
    - 체크박스 및 모달 구현
    - 애니메이션 위한 이미지 추가
    - 결과 모달에 애니메이션 적용
    - 인풋 내용 전송 fetch 구현 (post)

---
+++ public/javascripts/cooking.js
@@ -0,0 +1,41 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+    const checks = document.querySelectorAll('.input-form__check');
+    checks.forEach(elem=>elem.querySelector('input').addEventListener('click', (event)=>selectType(event)))
+
+    const submit = document.querySelector('.input-form__submit');
+    submit.addEventListener('click', createCookie);
+});
+
+const createCookie = ()=>{
+    const contents = document.querySelector('.input-form__text').value;
+    if (contents=='') return;
+
+    document.querySelector('.input-form__result__modal').classList.remove('hidden');
+    document.querySelector('.input-form__result__cookie').classList.add('animation');
+    
+    let type;
+    document.querySelectorAll('.input-form__check').forEach(elem=>{
+        if(elem.querySelector('input').checked) 
+            type=elem.querySelector('input').value;
+    });
+    type = +type;
+
+    sendCookie(type, contents);
+}
+
+const sendCookie = async(type, contents)=>{
+    let response = await fetch('./create-cookie', {
+        method: 'post', 
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify({"type":type, "contents":contents})
+    });
+    
+    let result;
+    if (response.ok)
+        result = await response.json();
+}
+
+const selectType = (event)=>{
+    document.querySelectorAll('.input-form__check').forEach(elem=>elem.querySelector('input').checked = false);
+    event.target.checked = true;
+}
