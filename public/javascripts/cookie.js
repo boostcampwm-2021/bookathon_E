424406bf03f2abb398eb1feb96ca56218267fc88
J190_정현구 <jungheun99@naver.com>
2021-09-23 21:53:02 +0900

    [Feat] 매운맛 쿠키 화면 생성
    layout.pug에서 cookie.css를 linking
    hot.pug에서 cookie.js를 script로 추가
    
    포춘쿠키 레이아웃 및 애니메이션 codepen 사이트 참고
    참고 사이트 : https://codepen.io/vlemoine/pen/dybRwNr

---
+++ public/javascripts/cookie.js
@@ -0,0 +1,47 @@
+const cookieWrapper = document.getElementById('fortuneCookie');
+const cookie = document.getElementById('cookie');
+const getNew = document.getElementById('new');
+const flip = document.getElementById('flip');
+const fortune = document.body.querySelector('.fortune');
+const numbers = document.body.querySelector('.numbers');
+const paper = document.body.querySelector('.paper-wrapper');
+const speed = 500;
+
+// Todo : fetch 사용해서 랜덤으로 말 뽑아오면 됨
+const fetchFortune = () => {
+    fortune.innerHTML = "매운맛";
+    numbers.innerHTML = 'Lucky numbers : 7';
+}
+
+const showFortune = () => {
+  cookieWrapper.classList.add('open');
+  cookie.disabled = true;
+};
+
+fetchFortune();
+
+cookie.addEventListener('click', showFortune, false);
+
+flip.addEventListener('click', e => {
+  paper.classList.toggle('flip');
+  flip.classList.toggle('front');
+  flip.classList.toggle('back');
+});
+
+getNew.addEventListener('click', e => {
+  cookieWrapper.classList.add('clear');
+  window.setTimeout(() => {
+    paper.classList.remove('flip');
+    flip.className = 'small front';
+    cookieWrapper.classList.remove('clear');
+    cookieWrapper.classList.remove('open');
+    cookieWrapper.classList.add('drop');
+    window.setTimeout(() => {
+      cookieWrapper.classList.remove('drop');
+      fortune.innerHTML = '';
+      numbers.innerHTML = '';
+      fetchFortune();
+    }, speed);
+  }, speed);
+  cookie.disabled = false;
+}, false);
