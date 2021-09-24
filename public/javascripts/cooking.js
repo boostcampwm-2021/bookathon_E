e8d8dc1f38d55d634bf788fc1b8444f5e6b1d2f5
Subin Min <tnqls5417@naver.com>
2021-09-24 13:15:31 +0900

    [FEAT] cooking 결과 모달 애니메이션 이미지 추가 처리
    - type hot일시 다른 이미지로 변경 (cookie-hot.png)
    - 기본 이미지 (cookie.png)

--- public/javascripts/cooking.js
+++ public/javascripts/cooking.js
@@ -9,17 +9,18 @@ window.addEventListener('DOMContentLoaded', ()=>{
 const createCookie = ()=>{
     const contents = document.querySelector('.input-form__text').value;
     if (contents=='') return;
-
-    document.querySelector('.input-form__result__modal').classList.remove('hidden');
-    document.querySelector('.input-form__result__cookie').classList.add('animation');
-    document.querySelector('.input-form').classList.add('hidden');
-
+    
     let type;
     document.querySelectorAll('.input-form__check').forEach(elem=>{
         if(elem.querySelector('input').checked) 
             type=elem.querySelector('input').value;
     });
     type = +type;
+    if (type==2)
+        document.querySelector('.input-form__result__cookie').src = '../images/cookie-hot.png';
+    document.querySelector('.input-form__result__modal').classList.remove('hidden');
+    document.querySelector('.input-form__result__cookie').classList.add('animation');
+    document.querySelector('.input-form').classList.add('hidden');
 
     sendCookie(type, contents);
 }
